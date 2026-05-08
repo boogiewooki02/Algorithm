f9ca1784da6d621519d670e9773192f5f9527d9e
Dongwook Kim <boogiewooki02@gmail.com>
2026-05-08 15:31:57 +0900

    [level 2] Title: 피로도, Time: 29.29 ms, Memory: 15 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -5,9 +5,9 @@ def solution(k, dungeons):
     perms = list(map(list, permutations(idx_list)))
     # print(perms)
     result = []
-    for perm in perms:
-        tmp = k
-        cnt = 0
+    for perm in perms: # 각 순열 케이스를 순회
+        tmp = k # 현재 피로도로 초기화
+        cnt = 0 # 각 순열마다의 결과
         for i in perm:
             if tmp >= dungeons[i][0]:
                 tmp -= dungeons[i][1]
